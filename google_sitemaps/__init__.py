4f53a36b6d94eda0978d961103cbc80ad277d591
Scott Rubin <srubin@broadway.com>
2018-03-21 14:30:56 -0400

    fixed app registry error

--- google_sitemaps/__init__.py
+++ google_sitemaps/__init__.py
@@ -1,8 +1,6 @@
-from django.conf import settings
-from .sitemaps import GoogleSitemap
-
 registry = {}
 
+
 def register(**kwargs):
-    for name,sitemap in kwargs.items():
+    for name, sitemap in kwargs.items():
         registry[name] = sitemap
